7f0f55f63354bfcd77b13e4430f72cf68c1d877a
FedeAlfaro <71413273+FedeAlfaro@users.noreply.github.com>
2023-10-25 23:17:17 -0600

    Update Maqueta.R
    Se eliminaron las tildes

--- Maqueta.R
+++ Maqueta.R
@@ -10,13 +10,13 @@ indicadoresPobreza <- read_excel("3 Dimensiones.xlsx")
 
 #Separamos los datos por variable
 datosSalud <- indicadoresPobreza$Salud
-datosEstandarVida <- indicadoresPobreza$`Estándares de Vida`
-datosEducacion <- indicadoresPobreza$Educación
+datosEstandarVida <- indicadoresPobreza$`Estandares de Vida`
+datosEducacion <- indicadoresPobreza$Educacion
 
 # Scatter plot: Salud-Educacion
 plot_Salud_Educacion <- ggplot(data = indicadoresPobreza, aes(x = datosSalud, y = datosEducacion)) +
   geom_point(colour="#3182bd", alpha = 0.8) +
-  labs(x = "Salud", y = "Educación")
+  labs(x = "Salud", y = "Educacion")
 options(repr.plot.width = 6, repr.plot.height = 4)
 
 print(plot_Salud_Educacion)
@@ -24,7 +24,7 @@ print(plot_Salud_Educacion)
 # Scatter plot: Educacion - Estandar de Vida
 plot_Educacion_EstandarVida <- ggplot(data = indicadoresPobreza, aes(x = datosEduacion, y = datosEstandarVida)) +
   geom_point(colour="#319b1d",alpha = 0.8) +
-  labs(x = "Educación", y = "Estádar Vida")
+  labs(x = "Educacion", y = "Estadar Vida")
 options(repr.plot.width = 6, repr.plot.height = 4)
 
 print(plot_EstandarVida_Educacion)
@@ -32,26 +32,26 @@ print(plot_EstandarVida_Educacion)
 # Scatter plot: Estandares de Vida-Salud
 plot_EstandarVida_Educacion <- ggplot(data = indicadoresPobreza, aes(x = datosEstandarVida, y = datosSalud)) +
   geom_point(colour="#623397",alpha = 0.8) +
-  labs(x = "Estándar de Vida", y = "Salud")
+  labs(x = "Estandar de Vida", y = "Salud")
 options(repr.plot.width = 6, repr.plot.height = 4)
 
 print(plot_EstandarVida_Educacion)
 
 
-# Visualización del plot 1x3
+# Visualizacion del plot 1x3
 layout(matrix(1:3, nrow = 1))
 par(mar = c(5, 4, 4, 2))
 
-# Plot 1: Distribución Salud
-hist(datosSalud, breaks = 20, col = "#3182bd", main = "Distribución", xlab = "Salud", ylab = "Valor")
+# Plot 1: Distribucion Salud
+hist(datosSalud, breaks = 20, col = "#3182bd", main = "Distribucion", xlab = "Salud", ylab = "Valor")
 abline(h = -0.01, col = "black", lty = 1)
 
-# Plot 2: Distribución Educación
-hist(datosEducacion, breaks = 20, col = "#319b1d", main = "Distribución", xlab = "Educación", ylab = "Valor")
+# Plot 2: Distribucion Educacion
+hist(datosEducacion, breaks = 20, col = "#319b1d", main = "Distribucion", xlab = "Educacion", ylab = "Valor")
 abline(h = -0.01, col = "black", lty = 1)
 
-# Plot 3: Distribución Estándar Vida
-hist(datosEstandarVida, breaks = 20, col = "#623397", main = "Distribución", xlab = "Estándar Vida", ylab = "Valor")
+# Plot 3: Distribucion Estandar Vida
+hist(datosEstandarVida, breaks = 20, col = "#623397", main = "Distribucion", xlab = "Estandar Vida", ylab = "Valor")
 abline(h = -0.01, col = "black", lty = 1)
 
 # Pruebas Shapiro-Wilk
@@ -60,20 +60,20 @@ shapiro_test_salud <- shapiro.test(datosSalud)
 shapiro_test_educacion <- shapiro.test(datosEducacion)
 shapiro_test_vida <- shapiro.test(datosEstandarVida)
 cat("Variable Salud: p-value =", shapiro_test_salud$p.value, "\n",
-"Variable Educación: p-value =", shapiro_test_educacion$p.value, "\n",
-"Variable Estándar de Vida: p-value =", shapiro_test_vida$p.value, "\n")
+"Variable Educacion: p-value =", shapiro_test_educacion$p.value, "\n",
+"Variable Estandar de Vida: p-value =", shapiro_test_vida$p.value, "\n")
 
 
 
 # Pruebas de Normalidad
 kolmogorov_test_salud <- lillie.test(datosSalud)
-cat("Variable Salud: Estadístico =", kolmogorov_test_salud$statistic, ", p-value =", kolmogorov_test_salud$p.value, "\n")
+cat("Variable Salud: Estadistico =", kolmogorov_test_salud$statistic, ", p-value =", kolmogorov_test_salud$p.value, "\n")
 
 kolmogorov_test_educacion <- lillie.test(datosEducacion)
-cat("Variable Educación: Estadístico =", kolmogorov_test_educacion$statistic, ", p-value =", kolmogorov_test_educacion$p.value, "\n")
+cat("Variable Educacion: Estadistico =", kolmogorov_test_educacion$statistic, ", p-value =", kolmogorov_test_educacion$p.value, "\n")
 
 kolmogorov_test_vida <- lillie.test(datosEstandarVida)
-cat("Variable Estándar de Vida: Estadístico =", kolmogorov_test_vida$statistic, ", p-value =", kolmogorov_test_vida$p.value, "\n")
+cat("Variable Estandar de Vida: Estadistico =", kolmogorov_test_vida$statistic, ", p-value =", kolmogorov_test_vida$p.value, "\n")
 
 # Correlacion de Spearman
 cor_Salud_Educacion <- corr.test(datosSalud, datosEducacion, method = "spearman")
@@ -90,7 +90,7 @@ set.seed(123)
 num_simulaciones <- 1000
 
 #-------------------------------------------------------------------------------
-# Salud - Educación
+# Salud - Educacion
 
 # Vector para almacenar los coeficientes de correlacion simulados
 coefs_Salud_Educacion <- numeric(num_simulaciones)
@@ -104,11 +104,11 @@ for (i in 1:num_simulaciones) {
 }
 
 # Histograma de los coeficientes de correlacion simulados
-hist(coefs_Salud_Educacion, breaks = 30, col = "#3182bd", main = "Dist. Coef. Correlación Método Bootstrap", xlab = "Salud - Educación", ylab = "Valor")
+hist(coefs_Salud_Educacion, breaks = 30, col = "#3182bd", main = "Dist. Coef. Correlacion Metodo Bootstrap", xlab = "Salud - Educacion", ylab = "Valor")
 abline(h = 0, col = "black", lty = 1)
 
 #-------------------------------------------------------------------------------
-# Educación - Estándar de Vida
+# Educacion - Estandar de Vida
 
 # Vector para almacenar los coeficientes de correlacion simulados
 coefs_Educacion_EstandarVida <- numeric(num_simulaciones)
@@ -122,12 +122,12 @@ for (i in 1:num_simulaciones) {
 }
 
 # Histograma de los coeficientes de correlacion simulados
-hist(coefs_Educacion_EstandarVida, breaks = 30, col = "#319b1d", main = "Dist. Coef. Correlación Método Bootstrap", xlab = "Educación - Estándar de Vida", ylab = "Valor")
+hist(coefs_Educacion_EstandarVida, breaks = 30, col = "#319b1d", main = "Dist. Coef. Correlacion Metodo Bootstrap", xlab = "Educacion - Estandar de Vida", ylab = "Valor")
 abline(h = 0, col = "black", lty = 1)
 
 
 #-------------------------------------------------------------------------------
-#Estándar de Vida - Salud
+#Estandar de Vida - Salud
 # Vector para almacenar los coeficientes de correlacion simulados
 coefs_EstandarVida_Salud <- numeric(num_simulaciones)
 
@@ -141,7 +141,7 @@ for (i in 1:num_simulaciones) {
 }
 
 # Histograma de los coeficientes de correlacion simulados
-hist(coefs_EstandarVida_Salud, breaks = 30, col = "#623397", main = "Dist. Coef. Correlación Método Bootstrap", xlab = "Estándar de Vida - Salud", ylab = "Valor")
+hist(coefs_EstandarVida_Salud, breaks = 30, col = "#623397", main = "Dist. Coef. Correlacion Metodo Bootstrap", xlab = "Estandar de Vida - Salud", ylab = "Valor")
 abline(h = 0, col = "black", lty = 1)
 
 
